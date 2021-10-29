82f98c17f185824dbd6f28dbac4776e9ed301931
0B6573649 <0B6573649@LAPTOP-EG72POLU>
2021-10-29 16:58:14 -0400

    Add project files.

---
+++ SuperBankAssistant/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuperBankAssistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
